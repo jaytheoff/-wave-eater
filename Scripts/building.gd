58a12250b7527fb26b49cbbc42d792f4b71d6070
jaytheoff <bronzeoffjay123@gmail.com>
2025-11-08 21:22:20 -0500

    buildings work well
    u can now work well in the game with the backgrounds

--- Scripts/building.gd
+++ Scripts/building.gd
@@ -4,10 +4,14 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play("Rise")
-	await get_tree().create_timer(1).timeout
-	$AnimatedSprite2D.play("Sink")
-
+	await get_tree().create_timer(5).timeout
+	_sink()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _sink():
+	$AnimatedSprite2D.play("Sink")
+	await get_tree().create_timer(1).timeout
+	queue_free()
