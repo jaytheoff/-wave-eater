8f7074c968f58cd7369c09399ff753a8e0cf3977
jaytheoff <bronzeoffjay123@gmail.com>
2025-11-08 20:59:36 -0500

    added building update
    making buildings

--- game.gd
+++ game.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var timer := 0.0
-
+var building = preload("res://Building.tscn")
 func _ready() -> void:
 	$"CanvasLayer/Game Over".hide()
 	$Timer.start()
@@ -20,6 +20,12 @@ func _on_Timer_timeout() -> void:
 	timer += 1
 	
 
-
 func _on_retry_pressed() -> void:
 	get_tree().reload_current_scene()
+
+
+func _spawn_building() -> void:
+	var bld_instance = building.instantiate()
+	var x_pos = randf_range(0, get_viewport_rect().size.x)
+	bld_instance.position = Vector2(x_pos, -50)
+	add_child(bld_instance)
