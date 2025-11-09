892d403ba0001d6ef05a6659df9fdd584bc690d3
jaytheoff <bronzeoffjay123@gmail.com>
2025-11-08 21:13:23 -0500

    addding animations
    adding animation to buildings

---
+++ Scripts/building.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.play("Rise")
+	await get_tree().create_timer(1).timeout
+	$AnimatedSprite2D.play("Sink")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
