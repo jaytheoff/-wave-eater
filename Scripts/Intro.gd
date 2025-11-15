3e31911e4f9866da58ee1f56ffce652472f5b781
jaytheoff <bronzeoffjay123@gmail.com>
2025-11-15 14:26:34 -0500

    game finished
    game finished only thing reminaing is trailer intro & sound

---
+++ Scripts/Intro.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	$Menu.hide()
+	$VideoStreamPlayer.play()
+
+
+func _on_video_stream_player_finished() -> void:
+	get_tree().change_scene("Scenes/Title Screen.tscn")
